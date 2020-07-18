74364c1ae2b83ff57b44e69ec40a68d773f1cb49
Filip <2175499M@student.gla.ac.uk>
2020-07-18 21:46:47 +0100

    Normalize velocity and thrust data before training and validation.

--- VAE.py
+++ VAE.py
@@ -158,8 +158,10 @@ if __name__ == "__main__":
     velocity_list, thrust_list, torque_list = get_lists()
     if data_type == "velocity":
         data = velocity_list
+        data[:] = [x * 10 for x in data]
     elif data_type == "thrust":
         data = thrust_list
+        data[:] = [x / 10 for x in data]
     elif data_type == "torque":
         data = torque_list
     else:
